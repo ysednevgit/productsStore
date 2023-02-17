78ecd25d4c9880102c3790edca469b9e6a2eac34
Yury Sednev <ysednev@gmail.com>
2023-02-16 19:08:06 -0600

    initial commit

---
+++ src/main/java/com/yury/productsStore/ProductsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.yury.productsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yury/productsStore/ProductsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yury.productsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
